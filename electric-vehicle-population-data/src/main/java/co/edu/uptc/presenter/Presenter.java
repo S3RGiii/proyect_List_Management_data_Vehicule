aa748bdaac9bb71d0b84c5028ced0db5e29792ad
JoseOrtegaUPTC <Jose.Ortega01@uptc.edu.co>
2024-09-12 23:49:56 -0500

    ajustes

--- electric-vehicle-population-data/src/main/java/co/edu/uptc/presenter/Presenter.java
+++ electric-vehicle-population-data/src/main/java/co/edu/uptc/presenter/Presenter.java
@@ -1,20 +1,21 @@
 package co.edu.uptc.presenter;
 
 import co.edu.uptc.model.GeographicAnalysis;
-import co.edu.uptc.view.MainView;
+import co.edu.uptc.view.GeographicAnalysisView;
 
 public class Presenter {
     
     private GeographicAnalysis model;
-    private MainView view;
+    private GeographicAnalysisView view;
 
-    public Presenter(GeographicAnalysis model, MainView view) {
+    public Presenter(GeographicAnalysis model, GeographicAnalysisView view) {
         this.model = model;
         this.view = view;
 
         // Pide los datos del modelo y los muestra en la vista
         //view.getTableByState().setModel(model.getRecordsByState());
-        //  view.getTableByCounty().setModel(model.getRecordsByCounty());
+        //view.getTableByCounty().setModel(model.getRecordsByCounty());
+        view.getCityLabel().setText("Ciudad con más vehículos: " + model.getCityWithMostVehicles());
     }
     
 }
